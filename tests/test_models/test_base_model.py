3ce77103d1cae3aed23ba0da4848b7de7b51ad05
Sue-wesh <shiroweru007@gmail.com>
2023-02-12 10:46:31 -0800

    test BaseModel

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -4,10 +4,15 @@
 import unittest
 import models
 from datetime import datetime
+from time import sleep
 from models.base_model import BaseModel
+
 
 class TestBaseModel(unittest.TestCase):
     """unittests for testing the BaseModel Class."""
+
+    def test_no_args(self):
+        self.assertEqual(BaseModel, type(BaseModel()))
 
     def test_new_instance(self):
         self.assertIn(BaseModel(), models.storage.all().values())
@@ -20,3 +25,20 @@ class TestBaseModel(unittest.TestCase):
 
     def test_updated_at(self):
         self.assertEqual(datetime, type(BaseModel().updated_at))
+
+    def test_two_models(self):
+        first = BaseModel()
+        secnd = BaseModel()
+        self.assertNotEqual(first.id, secnd.id)
+
+    def test_diff_creation(self):
+        first = BaseModel()
+        sleep(0.05)
+        secnd = BaseModel()
+        self.assertLess(first.created_at, secnd.created_at)
+
+    def test_diff_updation(self):
+        first = BaseModel()
+        sleep(0.05)
+        secnd = BaseModel()
+        self.assertLess(first.updated_at, secnd.updated_at)
